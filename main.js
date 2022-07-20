a101be4c3b6423040606c948aa8fd6633c37503e
20182418 <102075649+20182418@users.noreply.github.com>
2022-07-20 09:24:47 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+const typed= new Typed('.typed',{
+    strings:[
+        '<i class="place">INNOVACION</i>',
+        '<i class="place">INVESTIGACION</i>',
+        '<i class="place">CIENCIAS</i>',
+        '<i class="place">ARTE</i>',
+        '<i class="place">CREATIVIDAD</i>',
+        '<i class="place">ORIGINALIDAD</i>',
+        '<i class="place">HUMANIDADES</i>',
+    ],
+    stringsElement:'#cadenas-texto',
+    typeSpeed: 80,
+    startDelay:100,
+    backSpeed:75,
+    smartBackspace:true,
+    shuffle:false,
+    backDelay:1500,
+    loop:true,
+    showCursor: true,
+    cursorChar:'|',
+    contentType:'html'
+});
